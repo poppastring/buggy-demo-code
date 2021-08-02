8acfa3df222b0cb466788dfca56851d9c630f1a0
Mark Downie <poppastring@users.noreply.github.com>
2021-08-01 23:28:52 -0400

    Updated to look like a service

---
+++ src/aspnetcore/Services/LegacyService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuggyDemoCode.Services
+{
+    public class LegacyService
+    {
+        public async Task<string> DoAsyncOperationWell()
+        {
+            var random = new Random();
+
+            await Task.Delay(random.Next(10) * 1000);
+
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
