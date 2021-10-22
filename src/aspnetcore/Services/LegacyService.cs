1fc46e98ee8ed692926ccd5ef2805a0308a32dcd
Mark Downie <poppastring@users.noreply.github.com>
2021-10-22 17:26:20 -0400

    Correcting my awfule merge...

--- src/aspnetcore/Services/LegacyService.cs
+++ src/aspnetcore/Services/LegacyService.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BuggyDemoCode.Services
@@ -68,6 +69,74 @@ namespace BuggyDemoCode.Services
             {
                 await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
             };
+        }
+
+        public async Task SimpleDeadLockAsyncOperation()
+        {
+            var taskscomplete = new TaskCompletionSource<bool>();
+
+            Task? t2 = null;
+            Task? t1 = Task.Run(async () =>
+            {
+                await taskscomplete.Task;
+                await t2;
+            });
+
+            t2 = Task.Run(async () =>
+            {
+                await taskscomplete.Task;
+                await t1;
+            });
+
+            taskscomplete.SetResult(true);
+
+            await Task.WhenAll(t1, t2);
+        }
+
+        public async Task SemaphoreDeadLockAsyncOperations()
+        {
+            var semaphore = new SemaphoreSlim(1, 1);
+
+            await Task.Run(async () =>
+            {
+                await semaphore.WaitAsync();
+                await Task.Run(async () =>
+                {
+                    await semaphore.WaitAsync();
+                    semaphore.Release();
+                });
+            });
+
+            semaphore.Release();
+        }
+
+        public async Task ReallyBadYieldReturn()
+        {
+            object _syncObj = new object();
+
+            foreach (var i in GetEnum())
+            {
+                await UseAsync(i).ConfigureAwait(false);
+            }
+
+            IEnumerable<int> GetEnum()
+            {
+                lock (_syncObj)
+                {
+                    yield return GetValue(0);
+                    yield return GetValue(1);
+                }
+            }
+
+            int GetValue(int value)
+            {
+                lock (_syncObj)
+                {
+                    return value + 1;
+                }
+            }
+
+            Task UseAsync(int i) => Task.Delay(i + 5000);
         }
 
         public Task Transfer(Account fromaccount, Account toaccount, int sum)
