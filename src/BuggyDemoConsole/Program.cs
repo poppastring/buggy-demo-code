5ea3ae8e86d6c5fead9d4e9c0937d4b826b3e7da
Mark Downie <poppastring@users.noreply.github.com>
2021-11-01 18:52:17 -0400

    OOM, high GC

--- src/BuggyDemoConsole/Program.cs
+++ src/BuggyDemoConsole/Program.cs
@@ -1,16 +1,21 @@
 ﻿using BuggyDemoConsole.Models;
 
 
-
-Console.WriteLine("Press 1 or 2 please...");
+Console.WriteLine("Press the");
+Console.WriteLine("1) Crash - Null reference exceptions.");
+Console.WriteLine("2) Crash - GC Heap presssure, OOM Exceptions.");
 
 ConsoleKeyInfo keyReaded = Console.ReadKey();
 Console.WriteLine();
 
 switch (keyReaded.Key)
 {
-    case ConsoleKey.D1: //Number 1 Key
+    case ConsoleKey.D1: 
         NullReferenceException();
+        break;
+
+    case ConsoleKey.D2: //Number 1 Key
+        OutOfMemoryException();
         break;
 
     default: //Not known key pressed
@@ -25,4 +30,21 @@ static void NullReferenceException()
 {
     var f = new Foo();
     var name = f.Bar.Baz.Name;
+}
+
+
+static void OutOfMemoryException()
+{
+    List<Product> products = new List<Product>();
+    string answer = "";
+    do
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            products.Add(new Product(i, "product" + i));
+        }
+        Console.WriteLine("Leak some more? Y/N");
+        answer = Console.ReadLine().ToUpper();
+
+    } while (answer == "Y");
 }
