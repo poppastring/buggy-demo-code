a352551210299d0f1e2b24225c6e4779ce399cdb
Mark Downie <poppastring@users.noreply.github.com>
2021-10-31 22:30:55 -0400

    Updated to read keys to select the type of problem initiate.

--- src/BuggyDemoConsole/Program.cs
+++ src/BuggyDemoConsole/Program.cs
@@ -1,11 +1,28 @@
 ﻿using BuggyDemoConsole.Models;
 
 
-Console.ReadKey();
 
+Console.WriteLine("Press 1 or 2 please...");
 
-var f = new Foo();
-var name = f.Bar.Baz.Name;
+ConsoleKeyInfo keyReaded = Console.ReadKey();
+Console.WriteLine();
+
+switch (keyReaded.Key)
+{
+    case ConsoleKey.D1: //Number 1 Key
+        NullReferenceException();
+        break;
+
+    default: //Not known key pressed
+        Console.WriteLine("Wrong key, please try again.");
+        break;
+}
 
 Console.WriteLine("Hit any key to exit");
 Console.ReadKey();
+
+static void NullReferenceException()
+{
+    var f = new Foo();
+    var name = f.Bar.Baz.Name;
+}
