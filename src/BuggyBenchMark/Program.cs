c323e79509a332dee28c3c023e5168d63719f4e5
Mark Downie <poppastring@users.noreply.github.com>
2023-07-07 11:47:48 -0400

    Update

---
+++ src/BuggyBenchMark/Program.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Text;
+
+namespace BuggyBenchMark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ProcessingServices>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class ProcessingServices
+    {
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            //Write your initialization code here
+        }
+
+        [Benchmark]
+        public void ProcessTextBasedDataSource()
+        {
+            string someText = string.Empty;
+            for (int i = 0; i < 10_000; i++)
+            {
+                someText += "sometexttoconcatenate";
+            }
+        }
+    }
+}
